8399d046a45cbc1520bc6b9f3131e118f3cddc27
Richard Herrera <richard.herrera@nfl.com>
2015-11-03 20:59:54 -0800

    I don't see any performance gain in matching promises up front. Instead simply matching the route itself eliminates the dreaded white flash.

--- packages/react-wildcat-handoff/src/client.js
+++ packages/react-wildcat-handoff/src/client.js
@@ -17,34 +17,16 @@ function completeRender(cfg, routes) {
     }
 
     var component = clientRouter(cfg);
-    console.time("render");
 
     match(cfg, (error, redirectLocation, renderProps) => {
-        // return Promise.all(
-        //     renderProps.routes
-        //         .filter(route => Object.keys(route).some(key => (/^get/).test(key)))
-        //         .map(route => {
-        //             return Promise.all(
-        //                 Object.keys(route)
-        //                     .filter(key => (/^get/).test(key))
-        //                     .map(key => {
-        //                         console.log(key, route[key]);
-        //                         return new Promise(resolve => route[key](renderProps.location, resolve))
-        //                     })
-        //             );
-        //         })
-        // )
-        // .then(() => {
-            var reactRootElementID = window[__REACT_ROOT_ID__];
-            var reactRootElement = document.getElementById(reactRootElementID);
+        var reactRootElementID = window[__REACT_ROOT_ID__];
+        var reactRootElement = document.getElementById(reactRootElementID);
 
-            ReactDOM.render(component, reactRootElement);
+        ReactDOM.render(component, reactRootElement);
 
-            // Flag react as available
-            // This is a helpful hook for running tests
-            reactRootElement.setAttribute("data-react-available", true);
-            console.timeEnd("render");
-        // });
+        // Flag react as available
+        // This is a helpful hook for running tests
+        reactRootElement.setAttribute("data-react-available", true);
     });
 }
 
