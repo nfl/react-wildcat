fbfaf7f3173b4ffc340e2a658dcc41506b8fd983
Richard Herrera <richard.herrera@nfl.com>
2015-11-07 10:45:33 -0800

    Update example.

--- example/src/routes/ErrorExample/routes.js
+++ example/src/routes/ErrorExample/routes.js
@@ -1,12 +1,11 @@
+import ensure from "react-wildcat-ensure";
+
 // React router route
 export const path = "/error-example";
 
 // Lazy loaded components
-export async function getComponent(location, cb) {
-    try {
-        const Module = await System.import("./ErrorExample.js", module.id);
-        return cb(null, Module);
-    } catch (e) {
-        return cb(e);
-    }
+export function getComponent(location, cb) {
+    return ensure("./ErrorExample.js", module, (err, module) => {
+        return cb(err, module);
+    });
 }
--- example/src/routes/FlexboxExample/routes.js
+++ example/src/routes/FlexboxExample/routes.js
@@ -1,12 +1,11 @@
+import ensure from "react-wildcat-ensure";
+
 // React router route
 export const path = "/flexbox-example";
 
 // Lazy loaded components
-export async function getComponent(location, cb) {
-    try {
-        const Module = await System.import("./FlexboxExample.js", module.id);
-        return cb(null, Module);
-    } catch (e) {
-        return cb(e);
-    }
+export function getComponent(location, cb) {
+    return ensure("./FlexboxExample.js", module, (err, module) => {
+        return cb(err, module);
+    });
 }
