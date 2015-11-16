f8a2d489933a9afd479c0a810383b6fc88db0f8b
Richard Herrera <richard.herrera@nfl.com>
2015-11-16 11:39:34 -0800

    Work around ESLint false positive.

--- example/src/test/e2e/utils/sendIndividualKeys.js
+++ example/src/test/e2e/utils/sendIndividualKeys.js
@@ -1,5 +1,5 @@
-export default async function sendIndividualKeys(field, keys) {
+export default async (field, keys) => {
     for (const letter of keys.toString().split("")) {
         await field.sendKeys(letter);
     }
-}
+};
