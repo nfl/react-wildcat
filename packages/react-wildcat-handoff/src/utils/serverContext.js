84c46cf76bba5e33aaf22beba08f6ec1af8853dd
Richard Herrera <richard.herrera@nfl.com>
2015-11-30 14:14:08 -0800

    fix(react-wildcat-handoff): mirror client render and context
    Move some logic around to more closely mirror client rendering.

--- packages/react-wildcat-handoff/src/utils/serverContext.js
+++ packages/react-wildcat-handoff/src/utils/serverContext.js
@@ -4,7 +4,8 @@ const React = require("react");
 const Router = require("react-router");
 const RoutingContext = Router.RoutingContext;
 
-module.exports = function serverContext(request) {
+module.exports = function serverContext(request, renderProps) {
+    /* eslint-disable react/no-multi-comp */
     const ServerContext = React.createClass({
         childContextTypes: {
             radiumConfig: React.PropTypes.shape({
@@ -16,15 +17,15 @@ module.exports = function serverContext(request) {
             // Pass user agent to Radium
             return {
                 radiumConfig: {
-                    userAgent: request.header["user-agent"]
+                    userAgent: request.header["user-agent"] || "*"
                 }
             };
         },
 
         render() {
-            return React.createElement(RoutingContext, this.props);
+            return React.createElement(RoutingContext, Object.assign({}, this.props, renderProps));
         }
     });
 
-    return ServerContext;
+    return React.createElement(ServerContext);
 };
