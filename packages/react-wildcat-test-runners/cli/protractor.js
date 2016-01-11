a75e83338880d65f0eb5567ce615d81d63697d5c
Richard Herrera <richard.herrera@nfl.com>
2016-01-10 20:16:03 -0800

    refactor(react-wildcat-test-runners): update cli functionality
    Check for babel polyfill presence before requiring it

--- packages/react-wildcat-test-runners/cli/protractor.js
+++ packages/react-wildcat-test-runners/cli/protractor.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
-// "e2e": "npm run webdriver-update && protractor protractor.config.js",
-require("babel/register")({
+const path = require("path");
+var sep = (process.platform === "win32") ? ";" : ":";
+
+// Make protractor aware of module imports relative to src
+var nodeModulesPath = process.env.NODE_PATH || "";
+var srcPath = path.join(process.cwd(), "src");
+
+process.env.NODE_PATH = [nodeModulesPath, srcPath].join(sep);
+
+if (!global._babelPolyfill) {
+    require("babel/polyfill");
+}
+
+require("babel/register-without-polyfill")({
     retainLines: true,
-    sourceRoot: __dirname
+    sourceRoot: srcPath
 });
 
 // Use a require so we can do awesome ES7 stuff
