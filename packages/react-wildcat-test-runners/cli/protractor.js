520bbadba9081975e7e74c26d52fb7dce818f45c
Richard Herrera <richard.herrera@nfl.com>
2016-01-11 16:04:06 -0800

    fix(react-wildcat-test-runners): transpile module before publishing

--- packages/react-wildcat-test-runners/cli/protractor.js
+++ packages/react-wildcat-test-runners/cli/protractor.js
@@ -9,14 +9,4 @@ var srcPath = path.join(process.cwd(), "src");
 
 process.env.NODE_PATH = [nodeModulesPath, srcPath].join(sep);
 
-if (!global._babelPolyfill) {
-    require("babel/polyfill");
-}
-
-require("babel/register-without-polyfill")({
-    retainLines: true,
-    sourceRoot: srcPath
-});
-
-// Use a require so we can do awesome ES7 stuff
-require("../src/protractor.js");
+require("../dist/protractor.js");
