01d94ee0d2808663eb033faea051a9e1f8b4db72
Richard Herrera <richard.herrera@nfl.com>
2016-03-14 10:09:56 -0700

    perf(react-wildcat-handoff): Add client-side jspm_packages cache
    Implement solution provided here: https://github.com/jspm/jspm-cli/issues/872#issuecomment-196243931

--- packages/react-wildcat-handoff/src/utils/defaultTemplate.js
+++ packages/react-wildcat-handoff/src/utils/defaultTemplate.js
@@ -43,6 +43,92 @@ module.exports = function defaultTemplate(cfg) {
             });
         </script>
 
+        <script src="//npmcdn.com/dexie@1.3.3/dist/dexie.min.js"></script>
+        <script>
+            (function () {
+                var db = new Dexie("jspm");
+
+                db.version(1).stores({
+                    files: "&url,format,hash,contents"
+                });
+
+                db.open();
+
+                var log = console.error.bind(console);
+
+                function hash(str) {
+                    // Source: http://stackoverflow.com/a/7616484/502126
+                    var hash = 0, i, chr, len;
+
+                    if (str.length === 0) {
+                        return hash;
+                    }
+
+                    for (i = 0, len = str.length; i < len; i++) {
+                        chr = str.charCodeAt(i);
+                        hash = ((hash << 5) - hash) + chr;
+                        hash |= 0;
+                    }
+
+                    return hash.toString();
+                }
+
+                var cachedCall = function (loader, load, file, originalFunction, filter) {
+                    if (!filter || filter(load)) {
+                        return db.files.where("url").equals(file.url).first().then(function (cached) {
+                            if (!cached || cached.hash !== file.hash) {
+                                // console.log(file.url + " not in cache");
+
+                                return originalFunction.apply(loader, [load]).then(function (translated) {
+                                    file.format = load.metadata.format;
+                                    file.contents = translated;
+                                    return db.files.add(file).then(function () {
+                                        return translated;
+                                    }).catch(log);
+                                });
+                            }
+
+                            // console.log(file.url + " from cache");
+                            load.metadata.format = cached.format || undefined;
+                            return cached.contents;
+                        }).catch(log);
+                    }
+
+                    return originalFunction.apply(loader, [load]);
+                };
+
+                var onlyScriptDependencies = /\.*\\/jspm_packages\\/\.*\\.[tj]s/;
+
+                // override fetch
+                System.originalFetch = System.fetch;
+                System.fetch = function (load) {
+                    var file = {
+                        url: "load_" + load.address,
+                        hash: hash(load.address)
+                    };
+
+                    return cachedCall(this, load, file, System.originalFetch, function () {
+                        return load.address.search(onlyScriptDependencies) >= 0;
+                    });
+                };
+
+                // override translate
+                System.originalTranslate = System.translate;
+                System.translate = function (load) {
+                    if (!load.metadata.deps) {
+                        load.metadata.deps = []; // avoid https://github.com/systemjs/systemjs/pull/1158
+                    }
+
+                    var file = {
+                        url: "translate_" + load.address,
+                        hash: hash(load.source)
+                    };
+
+                    return cachedCall(this, load, file, System.originalTranslate);
+                };
+            }());
+        </script>
+
         <script>
             // FIXME: Possibly not needed in jspm 0.17
             // store the old normalization function
