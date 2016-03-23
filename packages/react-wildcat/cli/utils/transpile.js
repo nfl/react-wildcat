596af6947a5cf2aaae969b1f94c87eb3d0613935
Richard Herrera <richard.herrera@nfl.com>
2016-03-23 14:00:46 -0700

    feat(wildcat-babel): Add instrumentation support
    Add code coverage support to wildcat-babel

--- packages/react-wildcat/cli/utils/transpile.js
+++ packages/react-wildcat/cli/utils/transpile.js
@@ -4,13 +4,15 @@ const fs = require("fs-extra");
 const cwd = process.cwd();
 const pkg = require("../../package.json");
 const path = require("path");
-const slash = require("slash");
+// const slash = require("slash");
 const resolve = require("resolve");
 const notifier = require("node-notifier");
+const minimatch = require("minimatch");
 const pathExists = require("path-exists");
+const pathResolve = require("resolve-path");
 
 const Logger = require("../../src/utils/logger");
-const logger = new Logger("👀");
+const logger = new Logger("🔰");
 
 // Use project babel if found
 let babel;
@@ -37,10 +39,42 @@ if (pathExists.sync(babelRcPath)) {
 }
 
 module.exports = function transpile(commander) {
-    function log(msg) {
-        if (!commander.quiet) {
-            logger.meta(msg);
+    const wildcatConfig = require("../../src/utils/getWildcatConfig")(cwd);
+    const generalSettings = wildcatConfig.generalSettings;
+    const serverSettings = wildcatConfig.serverSettings;
+    const coverageSettings = generalSettings.coverageSettings;
+    // const logLevel = generalSettings.logLevel;
+
+    const coverage = generalSettings.coverage;
+    let instrumenter, instrumentationExcludes;
+
+    if (coverage) {
+        const coverageEnv = coverageSettings.env;
+        const coverageEnvSettings = coverageSettings[coverageEnv] || {};
+        const instrumentationSettings = coverageEnvSettings.instrumentation || {};
+
+        instrumentationExcludes = instrumentationSettings.excludes || [];
+
+        const coverageFiles = process.env.COVERAGE_FILES;
+        const suite = process.env.COVERAGE_SUITE;
+
+        if (coverageFiles) {
+            // Istanbul only supports excluding files,
+            // so the value of files must be converted to a negated exclusion
+            // i.e. --cover-files=foo.js,bar.js -> !{foo.js,bar.js}
+            const excludedFiles = [
+                coverageFiles.includes(",") ? `!{${coverageFiles.trim()}}` : `!${coverageFiles.trim()}`
+            ];
+
+            instrumentationExcludes = excludedFiles;
+        } else if (suite && instrumentationSettings.onSuiteExcludeCoverage) {
+            instrumentationExcludes = instrumentationExcludes.concat(
+                instrumentationSettings.onSuiteExcludeCoverage(suite)
+            );
         }
+
+        const Istanbul = require("istanbul");
+        instrumenter = new Istanbul.Instrumenter(instrumentationSettings);
     }
 
     function logError(err) {
@@ -55,11 +89,17 @@ module.exports = function transpile(commander) {
         // remove extension and then append back on .js
         relative = relative.replace(/\.(\w*?)$/, "") + ".js";
 
-        const dest = path.join(commander.outDir, relative);
+        const outDir = commander.outDir || serverSettings.outDir;
+        const sourceDir = commander.sourceDir || serverSettings.sourceDir;
+
+        const relativePath = path.join(outDir, relative);
+        const modulePath = pathResolve(cwd, relativePath);
+        const moduleSourcePath = pathResolve(cwd, relativePath.replace(outDir, sourceDir));
+        // const moduleBinPath = pathResolve(root, relativePath.replace(outDir, binDir));
 
         const dataOptions = Object.assign({}, babelOptions, {
-            sourceFileName: slash(path.relative(dest + "/..", src)),
-            sourceMapTarget: path.basename(relative)
+            sourceFileName: moduleSourcePath,
+            sourceMapTarget: path.basename(moduleSourcePath)
         });
 
         babel.transformFile(src, dataOptions, function transformData(transformErr, data) {
@@ -74,7 +114,7 @@ module.exports = function transpile(commander) {
                     sound: "Sosumi"
                 });
 
-                return done && done();
+                return done && done(transformErr);
             }
 
             if (data.ignored) {
@@ -84,19 +124,63 @@ module.exports = function transpile(commander) {
             const sourceMaps = babelOptions.sourceMaps || ((babelOptions.env || {}).development || {}).sourceMaps;
 
             if (data.map && sourceMaps && sourceMaps !== "inline") {
-                const mapLoc = dest + ".map";
+                const mapLoc = modulePath + ".map";
                 data.code = addSourceMappingUrl(data.code, mapLoc);
 
                 fs.createOutputStream(mapLoc)
+                    .on("error", function outputStreamError(outputErr) {
+                        logger.error(outputErr);
+                    })
                     .end(JSON.stringify(data.map));
             }
 
-            fs.createOutputStream(dest)
-                .on("finish", function outputStreamFinish() {
-                    log(src + " -> " + dest);
+            let instrumentedCodePromise;
+
+            if (
+                coverage &&
+                instrumenter &&
+                !instrumentationExcludes.some(minimatch.bind(null, relativePath))
+            ) {
+                instrumentedCodePromise = new Promise(
+                    function instrumenterPromise(resolveInstrumenter, rejectInstrumenter) {
+                        instrumenter.instrument(
+                            data.code,
+                            relativePath,
+                            function instrumentOutput(instrumentError, instrumentedCode) {
+                                if (instrumentError) {
+                                    return rejectInstrumenter(instrumentError);
+                                }
+
+                                return resolveInstrumenter(instrumentedCode);
+                            }
+                        );
+                    }
+                );
+            } else {
+                instrumentedCodePromise = Promise.resolve(data.code);
+            }
+
+            instrumentedCodePromise
+                .then(function instrumentedCodeResult(code) {
+                    fs.createOutputStream(modulePath)
+                        .on("error", function outputStreamError(outputErr) {
+                            logger.error(outputErr);
+                        })
+                        .end(code);
+
                     return done && done();
                 })
-                .end(data.code);
+                .catch(function instrumentedCodeError(err) {
+                    logger.error(err);
+                    return done && done(err);
+                });
+
+            // fs.createOutputStream(dest)
+            //     .on("finish", function outputStreamFinish() {
+            //         log(src + " -> " + dest);
+            //         return done && done();
+            //     })
+            //     .end(data.code);
         });
     };
 };
