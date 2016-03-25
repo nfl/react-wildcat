95898dcb72ef0089fe283b96c61a0d97e51f9b19
Richard Herrera <richard.herrera@nfl.com>
2016-03-25 09:19:58 -0700

    style(react-wildcat): Update blue box of death
    Update BBOD styling and output, adding more metadata output and making it more readable and
    mobile-friendly.

--- packages/react-wildcat/src/utils/blueBoxOfDeath.js
+++ packages/react-wildcat/src/utils/blueBoxOfDeath.js
@@ -1,19 +1,34 @@
 module.exports = function blueBoxOfDeath(err, request) {
+    const path = require("path");
+    const wildcatPkg = require(path.resolve(__dirname, "../../package.json"));
+
+    const name = wildcatPkg.name;
+    const version = wildcatPkg.version;
+    const dependencies = wildcatPkg.dependencies;
+    const author = wildcatPkg.author;
+    const license = wildcatPkg.license;
+
     const bbod = `
 <!doctype html>
 <html>
     <head>
-        <title>jspm import error</title>
+        <title>‼️💩️️ Runtime Error️</title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0;">
         <style>
-            .bbod {
+            .bbod, .bbod * {
+                margin: 0;
+                padding: 0;
+                line-height: 1.5em;
                 box-sizing: border-box;
+                border-radius: 3px;
+            }
+
+            .bbod {
+                margin: 0;
+                padding: 0;
                 font-family: sans-serif;
                 font-size: 1em;
-                position: fixed;
-                padding: 10px;
-                top: 0;
-                bottom: 0;
-                left: 0;
+                padding: 2em 3em;
                 width: 100%;
                 background: rgb(0, 102, 204);
                 color: white;
@@ -23,72 +38,93 @@ module.exports = function blueBoxOfDeath(err, request) {
                 font-weight: bold;
             }
 
-            .bbod ul {
-                font-weight: normal;
+            .bbod .info {
+                padding: 1.25em 1.5em;
+                margin-top: 1em;
+                border: 1px dashed rgba(0, 0, 0, 0.5);
+                background: rgba(255, 255, 255, 0.1);
             }
 
-            .bbod li {
-                margin-bottom: 2px;
+            .bbod ul {
+                margin: 1em 3em 0.25em;
+                font-weight: normal;
             }
 
             .bbod code {
                 display: inline-block;
                 background: rgba(0, 0, 0, 0.5);
-                border-radius: 2px;
-                padding: 2px 4px;
+                padding: 0.05em 0.4em;
                 cursor: default;
-                transition: all 0.15s linear;
-            }
-
-            .bbod li:hover code {
-                background: rgba(0, 0, 0, 0.9);
             }
 
             .bbod .stack {
-                margin-top: 30px;
-                padding-top: 20px;
+                margin-top: 1.5em;
+                padding-top: 1em;
                 border-top: 1px solid rgba(0, 0, 0, 0.5);
-                font-family: monospace;
-                overflow: auto;
             }
 
             .bbod .stack h3 {
-                margin: 5px 0 10px;
+                margin: 0.25em 0 0.5em;
             }
 
             .bbod .stack pre {
-                margin: 0;
-                padding: 10px;
+                margin: 0.5em 0 0;
+                padding: 1.25em 1.5em;
                 white-space: pre-wrap;
                 background: rgba(0, 0, 0, 0.5);
-                color: rgba(255, 255, 255, 0.8);
-                border-radius: 2px;
+                color: rgba(255, 255, 255, 0.9);
+                overflow: auto;
+                font: 0.95em 1em/1.5em "Operator Mono", monospace;
+                transition: all 0.15s linear;
+            }
+
+            .bbod .stack:hover pre {
+                background: rgba(0, 0, 0, 0.8);
+                color: rgba(255, 255, 255, 1);
             }
         </style>
     </head>
-    <body>
-        <div class="bbod">
-            <div class="message">
-                <p>An error occurred importing jspm packages for route <code>${request.url}</code>.</p>
-                <p>This is usually the result of attempting to load missing or invalid jspm package(s). Possible solutions:</p>
-                <ul>
-                    <li>Check your console for <code>40x</code> or <code>50x</code> status codes. These should identify missing packages or modules.</li>
-                    <li>Run <code>jspm install</code> to install any missing dependencies.</li>
-                </ul>
-            </div>
-            ${err ? `
-                <div class="stack">
-                    <h3>jspm error message:</h3>
-                    <pre>${err}</pre>
+    <body class="bbod">
+        <h1>Runtime Error</h1>
 
-                    <h3>jspm error stack trace:</h3>
-                    <pre>${err.stack}</pre>
-                </div>
-            ` : ""}
+        <div class="message">
+            <h3>An error occurred rendering route <code>${request.url}</code>.</h3>
+        </div>
+
+        <div class="message info">
+            <p>This may be the result of attempting to load missing or invalid jspm package(s). Possible solutions:</p>
+            <ul>
+                <li>Check your console for <code>40x</code> or <code>50x</code> status codes. These should identify missing packages or modules.</li>
+                <li>Run <code>jspm install</code> to install any missing dependencies.</li>
+            </ul>
+        </div>
+
+        ${err ? `
             <div class="stack">
-                <h3>server request payload:</h3>
-                <pre>${JSON.stringify(request, null, 4)}</pre>
+                <h4>Error Message:</h4>
+                <pre>${err}</pre>
             </div>
+
+            <div class="stack">
+                <h4>Stack Trace:</h4>
+                <pre>${err.stack}</pre>
+            </div>
+        ` : ""}
+
+        <div class="stack">
+            <h4>Server Request Payload:</h4>
+            <pre>${JSON.stringify(request, null, 4)}</pre>
+        </div>
+
+        <div class="stack">
+            <h4>Wildcat Metadata:</h4>
+            <pre>${JSON.stringify({
+                name,
+                version,
+                dependencies,
+                author,
+                license
+            }, null, 4)}</pre>
         </div>
     </body>
 </html>
