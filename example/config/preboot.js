6a6a60f0b60efda8ec2166947d89ae21b1b3dd34
James Hush <james.hush@nfl.com>
2016-06-22 14:36:00 -0700

    Updating preboot

--- example/config/preboot.js
+++ example/config/preboot.js
@@ -1,16 +1,15 @@
 const fs = require("fs");
 const preboot = require("preboot");
 
-preboot.getBrowserCode({
-    appRoot: "#content",    // selector for root element
-    freeze: "spinner",     // show spinner w button click & freeze page
-    replay: "rerender",    // rerender replay strategy
-    buffer: true,          // client app will write to hidden div until bootstrap complete
-    debug: true,
-    uglify: false,
-    presets: ["keyPress", "buttonPress", "focus"]
-}).then((r) => {
-    return fs.writeFile("static/preboot.js", r);
-}).catch((err) => {
+try {
+    const inlineCode = preboot.getInlineCode({
+        appRoot: "#content",    // selector for root element
+        buffer: false,
+        debug: true,
+        uglify: false
+    });
+
+    fs.writeFile("static/preboot.js", inlineCode);
+} catch (err) {
     console.error(err);
-});
+}
