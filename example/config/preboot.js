bdc6f2d4ae8fc9cd3218f313b511af1b60e953f8
James Hush <james.hush@nfl.com>
2016-07-06 15:29:47 -0700

    Added more event selectors

--- example/config/preboot.js
+++ example/config/preboot.js
@@ -10,13 +10,20 @@ try {
         buffer: false,
         debug: false,
         uglify: true,
-        eventSelectors: [{
-            selector: 'input[type="submit"],button',
-            events: ["click"],
-            preventDefault: true,
-            freeze: true
-        }]
-    });
+        eventSelectors: [
+            // for recording changes in form elements
+            {selector: "input,textarea", events: ["keypress", "keyup", "keydown", "input", "change"]},
+            {selector: "select,option", events: ["change"]},
+
+            // when user hits return button in an input box
+            {selector: "input", events: ["keyup"], preventDefault: true, keyCodes: [13], freeze: true},
+
+            // for tracking focus (no need to replay)
+            {selector: "input,textarea", events: ["focusin", "focusout", "mousedown", "mouseup"], noReplay: true},
+
+            // user clicks on a button
+            {selector: "input[type=\"submit\"],button", events: ["click"], preventDefault: true, freeze: true}
+        ]});
 
     fs.writeFile("static/preboot.js", inlineCode);
 } catch (err) {
