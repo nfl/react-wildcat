17901c3c5813af0ac169107579301794ad992108
James Hush <james.hush@nfl.com>
2016-08-29 14:03:51 -0700

    Fixed lint errors

--- packages/react-wildcat-ensure/src/test/browser/fixtures/array/AsyncExampleMixed.fixture.js
+++ packages/react-wildcat-ensure/src/test/browser/fixtures/array/AsyncExampleMixed.fixture.js
@@ -3,7 +3,7 @@ import React from "react";
 class AsyncExampleMixed extends React.Component {
     render() {
         return (
-            <div></div>
+            <div />
         );
     }
 }
--- packages/react-wildcat-ensure/src/test/browser/fixtures/single/AsyncExampleIndex.fixture.js
+++ packages/react-wildcat-ensure/src/test/browser/fixtures/single/AsyncExampleIndex.fixture.js
@@ -3,7 +3,7 @@ import React from "react";
 class AsyncExampleIndex extends React.Component {
     render() {
         return (
-            <div></div>
+            <div />
         );
     }
 }
