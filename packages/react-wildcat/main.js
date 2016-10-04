aa785d2c5022f6e1bcc8ce7b67f60bd7ad4e765e
James Hush <james.hush@nfl.com>
2016-10-04 11:27:25 -0700

    Remove heapdump package

--- packages/react-wildcat/main.js
+++ packages/react-wildcat/main.js
@@ -1,30 +1,5 @@
 "use strict";
 
-// function generateHeapDumpAndStats() {
-//     // 1. Force garbage collection every time this function is called
-//     try {
-//         global.gc();
-//     } catch (e) {
-//         console.log("You must run program with 'node --expose-gc index.js' or 'npm start'");
-//         process.exit();
-//     }
-
-//     // 2. Output Heap stats
-//     var heapUsed = process.memoryUsage().heapUsed;
-//     console.log("Program is using " + heapUsed + " bytes of Heap.")
-
-//     // 3. Get Heap dump
-//     process.kill(process.pid, "SIGUSR2");
-// }
-
-// function generateStats() {
-//     var heapUsed = process.memoryUsage().heapUsed;
-//     console.log("Program is using " + heapUsed + " bytes of Heap.")
-// }
-
-
-// setInterval(generateStats, 20000); // Do garbage collection and heap dump every 20 seconds
-
 // Better log
 require("better-log/install");
 
