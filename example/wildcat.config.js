5ad41e55c85550b3879735d70988988dda5aa299
James Hush <james.hush@nfl.com>
2016-10-04 17:22:21 -0700

    Check host name rather than docker

--- example/wildcat.config.js
+++ example/wildcat.config.js
@@ -5,11 +5,9 @@ const pkg = require("./package.json");
 const __DEV__ = (process.env.NODE_ENV === "development");
 const __PROD__ = (process.env.NODE_ENV === "production");
 const __BUNDLE__ = process.env.COVERAGE !== "e2e";
-const __IN_DOCKER__ = fs.existsSync("/.dockerenv");
-
 
 function getDefaultSSLFile(filename) {
-    const filePath = __IN_DOCKER__ ? "ssl/example." : "../packages/react-wildcat/ssl/server.";
+    const filePath = wildcatConfig.appServer.hostname === "localhost" ? "ssl/example." : "../packages/react-wildcat/ssl/server.";
     return fs.readFileSync(path.join(__dirname, `${filePath}${filename}`), "utf8");
 }
 
