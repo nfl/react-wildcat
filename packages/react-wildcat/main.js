3da7897fb011a2fd0cee1637830e35f65faf7b24
Richard Herrera <richard.herrera@nfl.com>
2017-02-03 17:52:40 -0800

    chore: remove polyfills

--- packages/react-wildcat/main.js
+++ packages/react-wildcat/main.js
@@ -3,12 +3,6 @@
 // Better log
 require("better-log/install");
 
-// Stub for Fetch API support
-require("./src/polyfills/fetch");
-
-// Stub for baseURI
-require("./src/polyfills/baseURI");
-
 require("./src/server").start();
 
 process.on("unhandledRejection", e => {
