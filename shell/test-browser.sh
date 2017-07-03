3c2b052059b2a2f89ac832781a02dafa6c211529
Luis De Avila <luis@ideanerd.com>
2017-07-03 10:26:01 -0700

    shell script changes to run jest

--- shell/test-browser.sh
+++ shell/test-browser.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-set -e
-
-TIMEOUT=10000
-
-if [ -n "$CI" ]; then
-    TIMEOUT=120000;
-fi
-
-karmaBin=node_modules/karma/bin/karma
-
-# Run browser test
-node ${karmaBin} start karma.config.js --log-level error --ui tdd --timeout ${TIMEOUT} --bail
+npm run test:browser
