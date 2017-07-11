09b208711bed20d144818af7025059411a40f2ca
Luis De Avila <luis@ideanerd.com>
2017-07-11 14:30:38 -0700

    package.json updates

---
+++ test/node.test.js
@@ -0,0 +1,5 @@
+// import "../packages/react-wildcat-handoff/src/test/browser/clientHandoffSpec.js";
+// import "../packages/react-wildcat-prefetch/src/test/browser/prefetchSpec.js";
+
+import "../packages/react-wildcat/test/appServerSpec.js"
+import "../packages/react-wildcat/test/staticServerSpec.js"
