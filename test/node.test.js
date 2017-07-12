667ac1e064e2209e83ce41a5a56eb92978e19d74
Luis De Avila <luis@ideanerd.com>
2017-07-12 11:53:07 -0700

    fixed timing issue

--- test/node.test.js
+++ test/node.test.js
@@ -1,6 +1,6 @@
 import "../packages/react-wildcat/test/memorySpec.js";
-import "../packages/react-wildcat/test/appServerSpec.js";
 import "../packages/react-wildcat/test/staticServerSpec.js";
 import "../packages/react-wildcat-handoff/test/serverHandoffSpec.js";
 import "../packages/react-wildcat-handoff/test/simpleHandoffSpec.js";
 import "../packages/react-wildcat-prefetch/test/prefetchSpec.js";
+import "../packages/react-wildcat/test/appServerSpec.js";
