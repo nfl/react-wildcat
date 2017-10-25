7c0a2f8baa44cbfe369cdcf7c5fbc558202ddde2
James Hush <james.hush@nfl.com>
2017-10-24 17:26:46 -0700

    Try this...

--- packages/react-wildcat/test/memorySpec.js
+++ packages/react-wildcat/test/memorySpec.js
@@ -22,7 +22,7 @@ describe("memory", () => {
 
         setTimeout(() => {
             memwatch.emit("stats");
-            expect(logger.info.callCount).to.equal(7);
+            expect(logger.info.callCount).to.equal(8);
             done();
         }, 1);
     });
