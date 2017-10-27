064110e6e9cd1e464e74c215fa679e182bd2668f
Richard Herrera <richard.herrera@nfl.com>
2017-10-26 17:26:43 -0700

    feat(react-wildcat-test-runners): Jest support

---
+++ packages/react-wildcat-test-runners/src/jest.js
@@ -0,0 +1,72 @@
+import url from "url";
+import path from "path";
+import yawn from "./utils/yawn.js";
+import cleanDirectory from "./utils/cleanDirectory.js";
+import checkServerStatus from "./utils/checkServerStatus.js";
+import startLocalServer from "./utils/startLocalServer.js";
+import startStaticServer from "./utils/startStaticServer.js";
+
+const cwd = process.cwd();
+
+const wildcatConfig = require(path.join(cwd, "wildcat.config.js"));
+
+const generalSettings = wildcatConfig.generalSettings;
+const coverageSettings = generalSettings.coverageSettings;
+const serverSettings = wildcatConfig.serverSettings;
+const appServerSettings = serverSettings.appServer;
+const staticServerSettings = serverSettings.staticServer;
+
+const originUrl = url.format({
+    protocol: appServerSettings.protocol.replace("http2", "https"),
+    hostname: appServerSettings.hostname,
+    port: appServerSettings.port
+});
+
+const staticUrl = url.format({
+    protocol: staticServerSettings.protocol.replace("http2", "https"),
+    hostname: staticServerSettings.hostname,
+    port: staticServerSettings.port
+});
+
+const args = process.argv
+    .slice(2)
+    .join(" ")
+    .trim();
+
+/* eslint-disable no-process-exit */
+export default (async () => {
+    try {
+        const origin = originUrl;
+        const shouldStartLocalServer = await checkServerStatus(origin);
+
+        const staticOrigin = staticUrl;
+        const shouldStartStaticServer = await checkServerStatus(staticOrigin);
+
+        const coverage = generalSettings.coverage;
+        const e2eSettings = coverageSettings.e2e;
+
+        let promises = [];
+
+        if (shouldStartLocalServer) {
+            promises = [...promises, startLocalServer()];
+        }
+
+        if (shouldStartStaticServer) {
+            promises = [...promises, startStaticServer()];
+        }
+
+        if (coverage) {
+            await cleanDirectory(e2eSettings.reporting.dir);
+        }
+
+        if (promises.length) {
+            await Promise.all(promises);
+        }
+
+        await yawn(`jest ${args}`);
+
+        process.exit();
+    } catch (e) {
+        process.exit(e);
+    }
+})();
